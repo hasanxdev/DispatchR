3861e57d2110b48440fb9bce1761d4a3f54a214b
ramin neynava <54467123+raminneynava@users.noreply.github.com>
2025-05-03 23:05:39 +0330

    Update DispatchRServiceCollection.cs

--- src/DispatchR/DispatchRServiceCollection.cs
+++ src/DispatchR/DispatchRServiceCollection.cs
@@ -40,4 +40,58 @@ public static class DispatchRServiceCollection
                 services.AddScoped(requestInterface.GetInterfaces().First(), handler);
             });
     }
+        public static void AddDispatchRV1(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddScoped<IMediator, Mediator>();
+
+        var openTypes = new[] { typeof(IRequestHandler<,>), typeof(IPipelineBehavior<,>) };
+        assembly.GetTypes()
+            .AsValueEnumerable()
+            .Where(t => t.IsClass && !t.IsAbstract)
+            .SelectMany(t => t.GetInterfaces()
+                .AsValueEnumerable()
+                .Where(i => i.IsGenericType && openTypes.Contains(i.GetGenericTypeDefinition()))
+                .Select(i => new { ServiceType = i, ImplementationType = t })).ToList()
+            .ForEach(x => services.AddScoped(x.ServiceType, x.ImplementationType));
+    }
+    public static void AddDispatchRV2(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddScoped<IMediator, Mediator>();
+
+        var openTypes = new[] { typeof(IRequestHandler<,>), typeof(IPipelineBehavior<,>) };
+
+        foreach (var type in assembly.GetTypes().AsValueEnumerable())
+        {
+            if (type.IsClass && !type.IsAbstract)
+            {
+                foreach (var @interface in type.GetInterfaces().AsValueEnumerable())
+                {
+                    if (@interface.IsGenericType && openTypes.Contains(@interface.GetGenericTypeDefinition()))
+                    {
+                        services.AddScoped(@interface, type);
+                    }
+                }
+            }
+        }
+    }
+    public static void AddDispatchRV3(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddScoped<IMediator, Mediator>();
+
+        var openTypes = new[] { typeof(IRequestHandler<,>), typeof(IPipelineBehavior<,>) };
+
+        foreach (var type in assembly.GetTypes())
+        {
+            if (type.IsClass && !type.IsAbstract)
+            {
+                foreach (var @interface in type.GetInterfaces())
+                {
+                    if (@interface.IsGenericType && openTypes.Contains(@interface.GetGenericTypeDefinition()))
+                    {
+                        services.AddScoped(@interface, type);
+                    }
+                }
+            }
+        }
+    }
 }
