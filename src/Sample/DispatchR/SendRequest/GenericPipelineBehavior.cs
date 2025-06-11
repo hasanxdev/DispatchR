511db8266a08b92ad73d54c62961c8853725b605
amin afrasiabi <99575913+aminafra@users.noreply.github.com>
2025-06-11 09:15:45 +0330

    Feat: add generic pipeline support to DispatchR (#15)
    - Introduced support for generic pipeline behaviors in DispatchR.
    - Updated `DispatchRServiceCollection` to handle generic pipeline types.
    - Added `GenericPipelineBehavior` implementation for both DispatchR and MediatR.
    - Registered generic pipeline behaviors in the sample application.

---
+++ src/Sample/DispatchR/SendRequest/GenericPipelineBehavior.cs
@@ -0,0 +1,16 @@
+﻿using DispatchR.Requests.Send;
+
+namespace Sample.DispatchR.SendRequest;
+
+public class GenericPipelineBehavior<TRequest, TResponse>(ILogger<GenericPipelineBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : class, IRequest<TRequest, TResponse>, new()
+{
+    public TResponse Handle(TRequest request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Generic Request Pipeline");
+        return NextPipeline.Handle(request, cancellationToken);
+    }
+
+    public required IRequestHandler<TRequest, TResponse> NextPipeline { get; set; }
+}
---
+++ src/Sample/MediatR/SendRequest/GenericPipelineBehavior.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Sample.MediatR.SendRequest;
+
+public class GenericPipelineBehavior<TRequest, TResponse>(ILogger<GenericPipelineBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Generic Request Pipeline");
+        return await next(cancellationToken);
+    }
+}
