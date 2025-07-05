47dd137d772bdac3df57c08acc7393aa01d1bcce
Matheus Ernan Reichert <matheus_ernan@outlook.com>
2025-07-04 22:17:08 -0300

    doc: XML summary

--- src/DispatchR/Configuration/ConfigurationOptions.cs
+++ src/DispatchR/Configuration/ConfigurationOptions.cs
@@ -8,7 +8,15 @@ namespace DispatchR.Configuration
         public bool RegisterNotifications { get; set; } = true;
         public List<Assembly> Assemblies { get; } = new();
         public List<Type>? PipelineOrder { get; set; }
+        /// <summary>
+        /// If null, this List is ignored. 
+        /// If set, only the specified handlers will be included.
+        /// </summary>
         public List<Type>? IncludeHandlers { get; set; }
+        /// <summary>
+        /// If null, this List is ignored. 
+        /// If set, only the specified handlers will be NOT included.
+        /// </summary>
         public List<Type>? ExcludeHandlers  { get; set; }
 
         public bool IsHandlerIncluded(Type handlerType)
