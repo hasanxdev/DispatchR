a42495baf939b3eddea3624d265ad45b5b1c596b
hasanxdev <hasan.arab.borzo@gmail.com>
2025-09-21 01:15:40 +0330

    support non-generic notifications

---
+++ src/DispatchR/Constants.cs
@@ -0,0 +1,7 @@
+namespace DispatchR;
+
+internal class Constants
+{
+    private const string DiagnosticPrefix = "DR";
+    internal const string DiagnosticPerformanceIssue = $"{DiagnosticPrefix}1000";
+}
